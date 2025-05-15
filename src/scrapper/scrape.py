10d5b6df69b98b07eae2ba91eb49041bd2282157
Akanksha0230 <akanksha012362@gmail.com>
2025-05-15 15:50:19 +0530

    some changes for deployment

--- src/scrapper/scrape.py
+++ src/scrapper/scrape.py
@@ -14,13 +14,26 @@ class ScrapeReviews:
     def __init__(self,
                  product_name:str,
                  no_of_products:int):
+         # Automatically install the correct chromedriver
+        chromedriver_autoinstaller.install()
+
         options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--window-size=1920,1080")
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        #options = Options()
         # options.add_argument("--no-sandbox")
         # options.add_argument("--disable-dev-shm-usage")
         # options.add_argument('--headless')
         
         # Start a new Chrome browser session
-        self.driver = webdriver.Chrome(options=options)
+        # self.driver = webdriver.Chrome(options=options)
 
         self.product_name = product_name
         self.no_of_products = no_of_products
