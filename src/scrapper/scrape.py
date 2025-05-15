4e11ca3bd18144e16590864f3eabdeb1afe41314
Akanksha0230 <akanksha012362@gmail.com>
2025-05-15 16:10:37 +0530

    fixing development errors

--- src/scrapper/scrape.py
+++ src/scrapper/scrape.py
@@ -22,8 +22,6 @@ class ScrapeReviews:
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--remote-debugging-port=9222")
         options.add_argument("--window-size=1920,1080")
 
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
